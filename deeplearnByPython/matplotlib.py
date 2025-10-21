3a1cedafef43f976eb67f2e7dcaae2229ceea3ac
甘增坤 <gan141425@163.com>
2025-10-21 19:22:18 +0800

    笔记提交

--- deeplearnByPython/matplotlib.py
+++ deeplearnByPython/matplotlib.py
@@ -2,12 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-x = np.arange(0, 10)
-
+arr = np.arange(1, 100000)
+reciprocals = [1/x for x in arr]
 # y = 1 / (1 + math.e ** (-x))
 # y = x ** 2
-y =  2 * x
+# y =  2 * x
+y = np.sin(reciprocals)
 
-plt.plot(x, y)
+plt.plot(reciprocals, y)
 
 plt.show()
